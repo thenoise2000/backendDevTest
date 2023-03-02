1132d1a7513e6bd5870ce143bdd590cb5a9263df
thenoise2000 <thenoise2000@gmail.com>
2023-03-02 11:23:32 -0400

    challenge rafael

--- MyAplication/src/main/java/com/MyAplication/model/Product.java
+++ MyAplication/src/main/java/com/MyAplication/model/Product.java
@@ -1,7 +1,5 @@
 package com.MyAplication.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
